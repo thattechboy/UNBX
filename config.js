70f882888ba05d1429ab795f5d733033a82b0afb
thattechboy <119555782+thattechboy@users.noreply.github.com>
2024-06-20 11:15:16 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   challenge: true, // Set to true if you want to enable password protection.
-  users: {
+  users: {UNBX
     // You can add multiple users by doing username: 'password'.
     llyinx: "404bitch",
   },
